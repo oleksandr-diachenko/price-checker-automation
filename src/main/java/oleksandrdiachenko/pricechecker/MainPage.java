9f054a13e34ec69da2fcd5c618d1541e21009870
oleksandr-diachenko <mail.positiv@gmail.com>
2020-05-07 10:59:50 +0300

    first commit

---
+++ src/main/java/oleksandrdiachenko/pricechecker/MainPage.java
@@ -0,0 +1,5 @@
+package oleksandrdiachenko.pricechecker;
+
+public class MainPage {
+
+}
---
+++ src/test/java/oleksandrdiachenko/pricechecker/MainPageTest.java
@@ -0,0 +1,10 @@
+package oleksandrdiachenko.pricechecker;
+
+import org.junit.jupiter.api.Test;
+
+public class MainPageTest {
+    @Test
+    public void openPage() {
+
+    }
+}
