3f1ae03a0f5d8b6552bdc7cd608422b1cdabb151
oleksandr-diachenko <mail.positiv@gmail.com>
2020-07-17 18:52:05 +0300

    Clean up

--- src/main/java/oleksandrdiachenko/pricechecker/pageobject/FileType.java
+++ src/main/java/oleksandrdiachenko/pricechecker/pageobject/FileType.java
@@ -2,10 +2,6 @@ package oleksandrdiachenko.pricechecker.pageobject;
 
 import lombok.Getter;
 
-import java.util.Arrays;
-
-import static java.lang.String.format;
-
 @Getter
 public enum FileType {
 
@@ -16,12 +12,5 @@ public enum FileType {
 
     FileType(String extension) {
         this.extension = extension;
-    }
-
-    public static FileType getByExtension(String extension) {
-        return Arrays.stream(FileType.values())
-                .filter(fileType -> fileType.extension.equalsIgnoreCase(extension))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(format("File type with extension %s not found", extension)));
     }
 }
--- src/main/java/oleksandrdiachenko/pricechecker/pageobject/MainPage.java
+++ src/main/java/oleksandrdiachenko/pricechecker/pageobject/MainPage.java
@@ -5,14 +5,11 @@ import com.codeborne.selenide.SelenideElement;
 import oleksandrdiachenko.pricechecker.annotation.RelativeUrl;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import static oleksandrdiachenko.pricechecker.helper.SelenidePageWrapper.page;
 import static org.openqa.selenium.By.id;
 
@@ -20,7 +17,6 @@ import static org.openqa.selenium.By.id;
 public class MainPage extends AbstractPage {
 
     private final SelenideElement file = $(id("file"));
-    private final SelenideElement fileLabel = $(id("fileInputLabel"));
     private final SelenideElement url = $(id("urlColumn"));
     private final SelenideElement insert = $(id("insertColumn"));
     private final SelenideElement check = $(id("check"));
@@ -32,31 +28,9 @@ public class MainPage extends AbstractPage {
         return page(MainPage.class);
     }
 
-    public boolean isCheckEnabled() {
-        return check.isEnabled();
-    }
-
-    public int getUrlInputValue() {
-        return Integer.parseInt(url.getValue());
-    }
-
-    public int getInsertInputValue() {
-        return Integer.parseInt(insert.getValue());
-    }
-
     public MainPage selectFile(File file) {
         this.file.uploadFile(file);
         return page(MainPage.class);
-    }
-
-    public String getTextFromFileSelector() {
-        return fileLabel.getText();
-    }
-
-    public Set<FileType> getAcceptableFileTypes() {
-        return Arrays.stream(file.getAttribute("accept").split(","))
-                .map(FileType::getByExtension)
-                .collect(toSet());
     }
 
     public MainPage setInsertInput(int value) {
